b26bf6d9cea9f3fbe599e93bb1a0b72a554c8529
chathunii <chathu38bbc@gmail.com>
2025-01-02 11:12:07 +0530

    changes for the new layout

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿// Shrinking banner script
+document.addEventListener("scroll", function () {
+    const banner = document.querySelector(".banner");
+    if (window.scrollY > 50) {
+        banner.classList.add("shrink");
+    } else {
+        banner.classList.remove("shrink");
+    }
+});
